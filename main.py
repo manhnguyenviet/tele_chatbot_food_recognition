67a67a0c1452b08f862065815ef17c2527e89fef
manhnv-glinteco <manh.nguyenviet@glinteco.com>
2023-12-12 15:48:49 +0700

    Update process image using trained model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
+import logging, os, json
 import numpy as np
 import cv2
+import tensorflow as tf
 
 from io import BytesIO
 from telegram import Update
@@ -16,11 +17,6 @@ from keras.models import load_model
 
 from settings import TELEGRAM_BOT_API_KEY, TELEGRAM_BOT_LINK
 from food_recognition import recognize_food
-from old_model import (
-    process_and_show_chart,
-    evaluate_result,
-    predict,
-)
 
 
 logging.basicConfig(
@@ -29,6 +25,36 @@ logging.basicConfig(
 
 
 use_trained_model = False
+
+
+def load_existed_model():
+    saved_folder_dir = "saved_models"
+    statistic_file_name = "training_statistics.json"
+    all_acc_statistics = []
+    subfolders = [f.name for f in os.scandir(saved_folder_dir) if f.is_dir()]
+    if not subfolders:
+        return None
+
+    for folder in subfolders:
+        version = f"{saved_folder_dir}/{folder}"
+        with open(f"{version}/{statistic_file_name}", "r") as f:
+            statistics = f.read()
+            json_statistics = json.loads(statistics)
+            acc_list = json_statistics["acc"]
+            acc_list.sort()
+            all_acc_statistics.append({"version": version, "acc": acc_list[-1]})
+
+    sorted_all_acc_statistics = sorted(all_acc_statistics, key=lambda d: d["acc"])
+    version = sorted_all_acc_statistics[-1]["version"]
+
+    model = load_model(version)
+    return model
+
+
+def load_class_names():
+    with open("class_names.json", "r") as f:
+        data = json.loads(f.read())
+        return data["names"]
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -74,18 +100,35 @@ async def google_cloud_vision_handle_photo(
     await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
 
 
-async def trained_model_handle_photo(
-    update: Update, context: ContextTypes.DEFAULT_TYPE
-):
-    reply_text = ""
-    # TODO: implement logic here
-    model = load_model("saved_models/food_recog_v1")
-    file = await context.bot.get_file(update.message.photo[-1].file_id)
-    """
-    should save the image in path, then use it to process the prediction
-    """
-    file.download_to_drive()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        file = await context.bot.get_file(update.message.photo[-1].file_id)
+        """
+        should save the image in path, then use it to process the prediction
+        """
+        img_path = "tele_img.jpg"
+        await file.download_to_drive(img_path)
+
+        model = load_existed_model()
+        img_height = 180
+        img_width = 180
+        img = tf.keras.utils.load_img(img_path, target_size=(img_height, img_width))
+        img_array = tf.keras.utils.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+        predictions = model.predict(img_array)
+        score = tf.nn.softmax(predictions[0])
+        class_names = load_class_names()
+
+        text = (
+            f"This image is most likely belongs to {class_names[np.argmax(score)]} with"
+            f" {round(100 * np.max(score), 4)}% confidence"
+        )
+        os.remove(img_path)
+    except Exception as ex:
+        text = f"Error loading and processing image: {ex}"
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 
 def main():
@@ -95,10 +138,7 @@ def main():
     echo_handler = CommandHandler("echo", echo)
     help_handler = CommandHandler("help", help)
     # help_handler = CommandHandler("help", help)
-    if not use_trained_model:
-        photo_handler = MessageHandler(filters.PHOTO, google_cloud_vision_handle_photo)
-    else:
-        photo_handler = MessageHandler(filters.PHOTO, trained_model_handle_photo)
+    photo_handler = MessageHandler(filters.PHOTO, handle_photo)
 
     # add the handlers to the bot
     application.add_handler(start_handler)
